dd0c14dcac55a5e74d0005be626bcf4f22c7ec4c
thisalhussein <118671459+thisalhussein@users.noreply.github.com>
2023-03-02 22:52:26 +0200

    add material app material app and add theme for make screen light

--- notes_project/lib/main.dart
+++ notes_project/lib/main.dart
@@ -9,6 +9,8 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData.light(),
+    );
   }
 }
